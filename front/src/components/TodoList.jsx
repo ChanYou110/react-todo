b9c23324958128348c32fc659bbd17e4dfa54c5a
ChanYou110 <mushoku110@gmail.com>
2024-06-03 23:20:07 +0900

    youtubeを真似してtodoアプリ作成

---
+++ front/src/components/TodoList.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export const TodoList = ({taskList, setTaskList}) => {
+
+    const handleDelete = (id) => {
+        // タスクを削除
+        setTaskList(taskList.filter((task) => task.id !== id));
+    }
+
+    const handleComplete = (id) => {
+        // タスクを完了にする
+        setTaskList(taskList.map((task) => {
+            if(id === task.id){
+                return {
+                    ...task,
+                    completed: !task.completed
+                };
+            }
+            return task;
+        }))
+    }
+
+    return (
+        <div className="todo">
+            <div className="todos">
+                {taskList.map((task, index) => (
+                    <div className={`todo ${task.completed ? "completed" : ""}`} key={index}>
+                        <div className="todoText">
+                            <span>{task.text}</span>
+                        </div>
+                        <div className="icons">
+                            <button onClick={() => handleComplete(task.id)}>
+                            <i className="fa-solid fa-check"></i>
+                            </button>
+                            <button onClick={() => handleDelete(task.id)}>
+                            <i className="fa-solid fa-trash"></i>
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+  )
+}
